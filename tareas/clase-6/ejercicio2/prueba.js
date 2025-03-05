69540368dabf2b7e0b0cc958381fe0f750a8673a
Alvarez Cristian <alvarezcristian21@gmail.com>
2025-03-05 01:16:27 -0300

    Utiliza prueba con console.assert
    Conviertiendo el objeto a array con sus valores y comparandolos con el
    mensaje de error o comillas simples

--- tareas/clase-6/ejercicio2/prueba.js
+++ tareas/clase-6/ejercicio2/prueba.js
@@ -1,6 +1,21 @@
 function probarValidarSalarios() {
-    const resultados = validarSalarios([0, -1, 1]);
+    const resultadoValidarSalarios = validarSalarios([0, -1, 1]);
+    const mensajes = Object.values(resultadoValidarSalarios);
 
+    console.assert(
+        mensajes[0] === 'El salario ingresado no puede ser cero',
+        'Error: validar salarios no valido con el número cero'
+    );
+
+    console.assert(
+        mensajes[1] === 'El salario ingresado no puede ser un número negativo',
+        'Error: validar salarios no valido con un número en negativo'
+    );
+
+    console.assert(
+        mensajes[2] === '',
+        'Error: validar salarios no valido con un número correcto'
+    );
 }
 
-//probarValidarSalarios()
+probarValidarSalarios();
